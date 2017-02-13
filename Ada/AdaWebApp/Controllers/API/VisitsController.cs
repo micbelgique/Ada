4ccd9a89a683a38632f387266336eb26fcb83ec7
Julien <Julien.Honnorez@outlook.fr>
2017-02-13 14:06:29 +0100

    config

---
+++ Ada/AdaWebApp/Controllers/API/VisitsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using AdaWebApp.Models.DAL;
+using AdaWebApp.Models.Entities;
+
+namespace AdaWebApp.Controllers.API
+{
+    public class VisitsController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/Visits
+        public IQueryable<Visit> GetVisits()
+        {
+            return db.Visits;
+        }
+
+        // GET: get visits of the day
+
+        // GET: api/Visits/5
+        [ResponseType(typeof(Visit))]
+        public IHttpActionResult GetVisit(int id)
+        {
+            Visit visit = db.Visits.Find(id);
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(visit);
+        }
+
+        // PUT: api/Visits/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutVisit(int id, Visit visit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != visit.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(visit).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VisitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Visits
+        [ResponseType(typeof(Visit))]
+        public IHttpActionResult PostVisit(Visit visit)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Visits.Add(visit);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = visit.Id }, visit);
+        }
+
+        // DELETE: api/Visits/5
+        [ResponseType(typeof(Visit))]
+        public IHttpActionResult DeleteVisit(int id)
+        {
+            Visit visit = db.Visits.Find(id);
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            db.Visits.Remove(visit);
+            db.SaveChanges();
+
+            return Ok(visit);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool VisitExists(int id)
+        {
+            return db.Visits.Count(e => e.Id == id) > 0;
+        }
+    }
+}
