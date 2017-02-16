5b1cfbda1c92015fe6e1afe31142b73b02cf689d
Louis <Louis@DESKTOP-GE60H5K>
2017-02-16 17:19:08 +0100

    ada fonctionne

--- Ada/AdaWebApp/Migrations/201702161500286_Initial.Designer.cs
+++ Ada/AdaWebApp/Migrations/201702161500286_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace AdaWebApp.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201702131101164_InitialCreate"; }
+            get { return "201702161500286_Initial"; }
         }
         
         string IMigrationMetadata.Source
--- Ada/AdaWebApp/Migrations/201702161500286_Initial.cs
+++ Ada/AdaWebApp/Migrations/201702161500286_Initial.cs
@@ -3,7 +3,7 @@ namespace AdaWebApp.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class Initial : DbMigration
     {
         public override void Up()
         {
