67095181dfb054b1616ee28862ea8660ad3fe736
Julie <Julie@DESKTOP-37L9IHG>
2017-02-17 16:03:17 +0100

    Hidden keys

--- Ada/AdaBot/Dialogs/AdaDialog.cs
+++ Ada/AdaBot/Dialogs/AdaDialog.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
+using AdaSDK;
 
 namespace AdaBot.Dialogs
 {
-    public class AdaDialog
+    [Serializable]
+    public class AdaDialog : LuisDialog<object>
     {
+        private Activity _message;
+
+        public AdaDialog(params ILuisService[] services): base(services)
+        {
+
+        }
+
+        protected override async Task MessageReceived(IDialogContext context, IAwaitable<IMessageActivity> item)
+        {
+            _message = (Activity)await item;
+            await base.MessageReceived(context, item);
+        }
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Je n'ai pas compris :/";
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+            message = $"Je suis constamment en apprentissage, je vais demander à mes administrateurs de me l'apprendre.";
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
     }
 }
