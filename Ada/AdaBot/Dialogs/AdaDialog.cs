220c2fe82d872d3f8b31c3ed8ee87ab61d79508b
Julie <Julie@DESKTOP-37L9IHG>
2017-02-21 16:08:12 +0100

    SDK OK

--- Ada/AdaBot/Dialogs/AdaDialog.cs
+++ Ada/AdaBot/Dialogs/AdaDialog.cs
@@ -22,7 +22,7 @@ namespace AdaBot.Dialogs
     {
         private static Activity _message;
 
-        public AdaDialog(params ILuisService[] services): base(services)
+        public AdaDialog(params ILuisService[] services) : base(services)
         {
 
         }
@@ -55,64 +55,54 @@ namespace AdaBot.Dialogs
         [LuisIntent("GetVisitsToday")]
         public async Task GetVisitsToday(IDialogContext context, LuisResult result)
         {
-            List<VisitDto> visits;
+            AdaClient client = new AdaClient();
+            List<VisitDto> visits = await client.GetVisitsToday();
 
-            using (var client = new HttpClient())
+            Activity replyToConversation;
+
+            if (visits.Count == 0)
             {
-                //ToDo Addapter URL
-                var httpResponse = await client.GetAsync(ConfigurationManager.AppSettings["ApiGetVisitsToday"]);
+                replyToConversation = _message.CreateReply("Je n'ai encore vu personne aujourd'hui... :'(");
+                replyToConversation.Recipient = _message.From;
+                replyToConversation.Type = "message";
+            }
+            else
+            {
+                replyToConversation = _message.CreateReply("Voici les visites du jour:");
+                replyToConversation.Recipient = _message.From;
+                replyToConversation.Type = "message";
+                replyToConversation.AttachmentLayout = "carousel";
+                replyToConversation.Attachments = new List<Attachment>();
 
-                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                foreach (var visit in visits)
                 {
-                    var x = await httpResponse.Content.ReadAsStringAsync();
-                    visits = JsonConvert.DeserializeObject<List<VisitDto>>(x);
-                    Activity replyToConversation;
+                    List<CardImage> cardImages = new List<CardImage>();
+                    cardImages.Add(new CardImage(url: $"{ ConfigurationManager.AppSettings["WebAppUrl"] }{VirtualPathUtility.ToAbsolute(visit.ProfilePicture.Uri)}"));
 
-                    if (visits.Count == 0)
+                    //Calcul la bonne année et la bonne heure.
+                    int wrongDate = visit.PersonVisit.DateVisit.Year;
+                    int goodDate = DateTime.Today.Year - wrongDate;
+
+                    HeroCard plCard = new HeroCard()
                     {
-                        replyToConversation = _message.CreateReply("Je n'ai encore vu personne aujourd'hui... :'(");
-                        replyToConversation.Recipient = _message.From;
-                        replyToConversation.Type = "message";
-                    }
-                    else
-                    {
-                        replyToConversation = _message.CreateReply("Voici les visites du jour:");
-                        replyToConversation.Recipient = _message.From;
-                        replyToConversation.Type = "message";
-                        replyToConversation.AttachmentLayout = "carousel";
-                        replyToConversation.Attachments = new List<Attachment>();
+                        Title = visit.PersonVisit.FirstName,
+                        Text = Convert.ToString(visit.PersonVisit.DateVisit.AddHours(1).AddYears(goodDate)),
+                        //Subtitle = 
+                        Images = cardImages
+                        //Buttons = cardButtons
+                    };
 
-                        foreach (var visit in visits)
-                        {
-                            List<CardImage> cardImages = new List<CardImage>();
-                            cardImages.Add(new CardImage(url: $"{ ConfigurationManager.AppSettings["WebAppUrl"] }{VirtualPathUtility.ToAbsolute(visit.ProfilePicture.Uri)}"));
-
-                            //Calcul la bonne année et la bonne heure.
-                            int wrongDate = visit.PersonVisit.DateVisit.Year;
-                            int goodDate = DateTime.Today.Year - wrongDate;
-
-                            HeroCard plCard = new HeroCard()
-                            {
-                                Title = visit.PersonVisit.FirstName,
-                                Text = Convert.ToString(visit.PersonVisit.DateVisit.AddHours(1).AddYears(goodDate)),
-                                //Subtitle = 
-                                Images = cardImages
-                                //Buttons = cardButtons
-                            };
-
-                            Attachment plAttachment = plCard.ToAttachment();
-                            replyToConversation.Attachments.Add(plAttachment);
-                        }
-                    }
-
-                    await context.PostAsync(replyToConversation);
-                    context.Wait(MessageReceived);
+                    Attachment plAttachment = plCard.ToAttachment();
+                    replyToConversation.Attachments.Add(plAttachment);
                 }
             }
+
+            await context.PostAsync(replyToConversation);
+            context.Wait(MessageReceived);
         }
 
         [LuisIntent("GetLastVisitPerson")]
-        public async Task GetLastVisitPersonHello(IDialogContext context, LuisResult result)
+        public async Task GetLastVisitPerson(IDialogContext context, LuisResult result)
         {
             string firstname = result.Entities[0].Entity;
             List<VisitDto> visits;
@@ -164,13 +154,11 @@ namespace AdaBot.Dialogs
                             replyToConversation.Attachments.Add(plAttachment);
                         }
                     }
-                    
+
                     await context.PostAsync(replyToConversation);
                     context.Wait(MessageReceived);
                 }
             }
         }
-
-
     }
 }
---
+++ Ada/AdaSDK/AdaClient.cs
@@ -0,0 +1,54 @@
+﻿using AdaSDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace AdaSDK
+{
+    public class AdaClient
+    {
+        public HttpClient HttpClient { get; set; }
+
+        public AdaClient()
+        {
+            HttpClient = new HttpClient();
+            HttpClient.MaxResponseContentBufferSize = 256000;
+        }
+
+        public async Task<List<VisitDto>> GetVisitsToday()
+        {
+            try
+            {
+                var response = await HttpClient.GetAsync(new Uri("http://adawebapp.azurewebsites.net/Api/Visits/VisitsToday"));
+                var content = await response.Content.ReadAsStringAsync();
+                var visits = JsonConvert.DeserializeObject<List<VisitDto>>(content);
+                return visits;
+            }
+            catch (Exception e)
+            {
+                // TODO : Propagate exception to caller
+                return new List<VisitDto>();
+            }
+        }
+
+        public async Task<List<VisitDto>> GetLastVisitPerson()
+        {
+            try
+            {
+                var response = await HttpClient.GetAsync(new Uri("http://adawebapp.azurewebsites.net/Api/Visits/LastVisitByFirstname"));
+                var content = await response.Content.ReadAsStringAsync();
+                var visits = JsonConvert.DeserializeObject<List<VisitDto>>(content);
+                return visits;
+            }
+            catch (Exception e)
+            {
+                // TODO : Propagate exception to caller
+                return new List<VisitDto>();
+            }
+        }
+    }
+}
