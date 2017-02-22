972246dc7a85ad42a6cb5155615ea21c76d2a483
Julie <Julie@DESKTOP-37L9IHG>
2017-02-22 16:08:33 +0100

    Dialog OK

--- Ada/AdaBot/Dialogs/AdaDialog.cs
+++ Ada/AdaBot/Dialogs/AdaDialog.cs
@@ -163,13 +163,13 @@ namespace AdaBot.Dialogs
 
             if (visits.Count == 0)
             {
-                replyToConversation = _message.CreateReply("Je n'ai pas encore rencontré " + firstname + " :/");
+                replyToConversation = _message.CreateReply("Je n'ai pas encore rencontré " + firstname + " :/ Il faudrait nous présenter! ^^");
                 replyToConversation.Recipient = _message.From;
                 replyToConversation.Type = "message";
             }
             else
             {
-                replyToConversation = _message.CreateReply("J'ai vu " + firstname + " à cette date:");
+                replyToConversation = _message.CreateReply("Voyons voir...");
                 replyToConversation.Recipient = _message.From;
                 replyToConversation.Type = "message";
                 replyToConversation.AttachmentLayout = "carousel";
@@ -181,13 +181,52 @@ namespace AdaBot.Dialogs
                     cardImages.Add(new CardImage(url: $"{ ConfigurationManager.AppSettings["WebAppUrl"] }{VirtualPathUtility.ToAbsolute(visit.ProfilePicture.Uri)}"));
 
                     //Calcul la bonne année et la bonne heure.
+                    DateTime today = DateTime.Today;
                     int wrongDate = visit.PersonVisit.DateVisit.Year;
                     int goodDate = DateTime.Today.Year - wrongDate;
+                    string messageDate = "";
+
+                    //Préparation du message du HeroCard en fonction de la date de la visite
+                    if (visit.PersonVisit.DateVisit.Day == today.Day)
+                    {
+                        if (visit.PersonVisit.DateVisit.Hour <= 12)
+                        {
+                            messageDate = "J'ai croisé " + firstname + " ce matin.";
+                        }
+                        else if (visit.PersonVisit.DateVisit.Hour >= 12 && visit.PersonVisit.DateVisit.Hour <= 17)
+                        {
+                            messageDate = "J'ai croisé " + firstname + " cet après-midi.";
+                        }
+                        else
+                        {
+                            messageDate = "J'ai croisé " + firstname + " cette nuit... Il doit sûrement faire des heures sup'!";
+                        }
+                    }
+                    else if (visit.PersonVisit.DateVisit.Day == today.Day - 1)
+                    {
+                        if (visit.PersonVisit.DateVisit.Hour <= 12)
+                        {
+                            messageDate = "J'ai croisé " + firstname + " hier matin.";
+                        }
+                        else if (visit.PersonVisit.DateVisit.Hour >= 12 && visit.PersonVisit.DateVisit.Hour <= 17)
+                        {
+                            messageDate = "J'ai croisé " + firstname + " hier après-midi.";
+                        }
+                        else
+                        {
+                            messageDate = "J'ai croisé " + firstname + " la nuit dernière... Il doit sûrement faire des heures sup'!";
+                        }
+                    }
+                    else
+                    {
+                        var dayDiff = visit.PersonVisit.DateVisit.Day - today.Day;
+                        messageDate = "J'ai croisé " + firstname + " il y a " + dayDiff + " jours.";
+                    }
 
                     HeroCard plCard = new HeroCard()
                     {
                         Title = visit.PersonVisit.FirstName,
-                        Text = Convert.ToString(visit.PersonVisit.DateVisit.AddHours(1).AddYears(goodDate)),
+                        Text = messageDate + "(" + Convert.ToString(visit.PersonVisit.DateVisit.AddHours(1).AddYears(goodDate)) + ")",
                         //Subtitle = 
                         Images = cardImages
                         //Buttons = cardButtons
