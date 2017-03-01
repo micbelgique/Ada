e668d33415c524d0446d53ee669aff0449fefd8d
Louis <Louis@DESKTOP-GE60H5K>
2017-03-01 11:12:18 +0100

    d

--- Ada/AdaBot/Dialogs/CreateDialog.cs
+++ Ada/AdaBot/Dialogs/CreateDialog.cs
@@ -16,7 +16,7 @@ namespace AdaBot.Dialogs
 
         public string GetVisitsMessage(string firstname, DateTime dateVisit)
         {
-            string message;
+            string message; 
 
             if (dateVisit.Day == DateTime.Today.Day)
             {
