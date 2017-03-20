df3cc117998a45e8b5e02690316ece91f2a63f23
Julie <Julie@JARVIS>
2017-03-20 10:28:37 +0100

    Best friend anonyme bug fixed terms of use page created in webApp

---
+++ Ada/AdaWebApp/Controllers/CGUController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AdaWebApp.Controllers
+{
+    public class CGUController : Controller
+    {
+        // GET: CGU
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Ada/AdaWebApp/Models/DAL/Repositories/VisitRepository.cs
+++ Ada/AdaWebApp/Models/DAL/Repositories/VisitRepository.cs
@@ -32,8 +32,12 @@ namespace AdaWebApp.Models.DAL.Repositories
         {
             DateTime date2 = DateTime.Today;
             DateTime date1 = date2.AddDays(-1);
-            int maxPasses = Table.Include(v => v.Person).Where(v => DbFunctions.TruncateTime(v.Date) >= DbFunctions.TruncateTime(date1) && DbFunctions.TruncateTime(v.Date) <= DbFunctions.TruncateTime(date2)).Max(v => v.NbPasses);
-            return Table.Include(v => v.Person).Where(v => DbFunctions.TruncateTime(v.Date) >= DbFunctions.TruncateTime(date1) && DbFunctions.TruncateTime(v.Date) <= DbFunctions.TruncateTime(date2)).First(v => v.NbPasses == maxPasses);
+            int maxPasses = Table.Include(v => v.Person).Where(v => DbFunctions.TruncateTime(v.Date) >= DbFunctions.TruncateTime(date1)
+            && DbFunctions.TruncateTime(v.Date) <= DbFunctions.TruncateTime(date2)
+            && v.Person.FirstName != null).Max(v => v.NbPasses);
+            return Table.Include(v => v.Person).Where(v => DbFunctions.TruncateTime(v.Date) >= DbFunctions.TruncateTime(date1) 
+            && DbFunctions.TruncateTime(v.Date) <= DbFunctions.TruncateTime(date2)
+            && v.Person.FirstName != null).First(v => v.NbPasses == maxPasses);
         }
 
         public List<Visit> GetVisitsByDate(DateTime date1, DateTime? date2)
