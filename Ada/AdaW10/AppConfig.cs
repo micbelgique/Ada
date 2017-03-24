32071cc167e34c0ed4a8d364e28318727779316e
louisvdd <louisvandendooren@gmail.com>
2017-03-24 12:50:12 +0100

    modification luis and gitignore

---
+++ Ada/AdaW10/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace AdaW10
+{
+    public static class AppConfig
+    {
+#if DEBUG
+        public static readonly string WebUri = "";      // AdaWebApp URL for Test
+#else
+        public static readonly string WebUri = "";      // AdaWebApp URL fro Prod
+#endif
+        public static readonly string UserName = "";    // Use to get the API token
+        public static readonly string Password = "";
+    }
+}
