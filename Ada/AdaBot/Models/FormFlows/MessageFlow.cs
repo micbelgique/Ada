461965eda7a74c9c75a546faad39ae0c752a529a
Julien <Julien.Honnorez@outlook.fr>
2017-04-07 10:45:35 +0200

    Confirmation in french

--- Ada/AdaBot/Models/FormFlows/MessageFlow.cs
+++ Ada/AdaBot/Models/FormFlows/MessageFlow.cs
@@ -15,6 +15,12 @@ namespace AdaBot.Models.FormFlows
         public static IForm<MessageFlow> BuildForm()
         {
             return new FormBuilder<MessageFlow>()
+                .Field(nameof(Message))
+                .Confirm(async (state) =>
+               {
+                   return new PromptAttribute($"Êtes-vous sûr de vouloir envoyer ce message: {state.Message.ToString()}");
+
+                   })
                     .Build();
         }
     }
