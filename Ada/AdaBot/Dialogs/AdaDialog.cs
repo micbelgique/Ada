22ef270371a6d8be867d1a12d7c2d2f4ae2c7c2c
Louisvdd <louisvandendooren@gmail.com>
2017-04-24 12:50:37 +0200

    Modification for web socket

--- Ada/AdaBot/Dialogs/AdaDialog.cs
+++ Ada/AdaBot/Dialogs/AdaDialog.cs
@@ -92,6 +92,8 @@ namespace AdaBot.Dialogs
             IMessageActivity newMessage = Activity.CreateMessageActivity();
             newMessage.Type = ActivityTypes.Message;
             newMessage.Recipient = MessagesController.from;
+            newMessage.Conversation = MessagesController.conversation;
+            newMessage.From = MessagesController.botAccount;
             newMessage.Text = "take picture";
             await connector.Conversations.SendToConversationAsync((Activity)newMessage);
         }
