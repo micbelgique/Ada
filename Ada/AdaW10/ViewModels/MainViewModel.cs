6f82750c26d4a5ae7dc9eb311710b9e76227b43b
Julien <Julien.Honnorez@outlook.fr>
2017-05-08 15:49:49 +0200

    test

--- Ada/AdaW10/ViewModels/MainViewModel.cs
+++ Ada/AdaW10/ViewModels/MainViewModel.cs
@@ -268,6 +268,9 @@ namespace AdaW10.ViewModels
             }
             else if (activity.Name != "NotFinish")
             {
+                LogHelper.Log("Que puis-je faire pour toi?");
+                await TtsService.SayAsync("Que puis-je faire pour toi?");
+
                 await DispatcherHelper.RunAsync(async () => { await SolicitExecute(); });
             }
         }
@@ -368,9 +371,6 @@ namespace AdaW10.ViewModels
             {
                 await WebcamService.StopFaceDetectionAsync();
             }
-
-            LogHelper.Log("Que puis-je faire pour toi?");
-            await TtsService.SayAsync("Que puis-je faire pour toi?");
 
             var str = await VoiceInterface.Listen();
             LogHelper.Log(str);
